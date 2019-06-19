d90647f97d04c28377866acd9de0a84c9368867d
Tyler Hozie <tyler.hozie@market-pl.com>
2019-06-19 16:09:33 -0500

    Update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,3 +1,15 @@
 jQuery(document).ready(function(){
 
+  $('#attending').change(function(){
+
+    if($(this).val() == 'Yes') {
+      $('.attending-guests').show();
+      $('.guest-options').show();
+    } else {
+      $('.attending-guests').hide();
+      $('.guest-options').hide();
+    }
+
+  });
+
 });
