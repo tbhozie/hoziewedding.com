75973419c94bcea35a31d4e5dde50288c75f8db3
Tyler Hozie <tyler.hozie@market-pl.com>
2019-06-19 16:20:27 -0500

    Update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -5,16 +5,15 @@ jQuery(document).ready(function(){
     if($(this).val() == 'Yes') {
       $('.attending-guests').show();
       $('.guest-options').show();
-      $('.attending-guests input').attr('required', TRUE);
     } else {
       $('.attending-guests').hide();
       $('.guest-options').hide();
-      $('.attending-guests input').attr('required', FALSE);
     }
 
   });
 
-  $('#addGuest').click(function(){
+  $('#addGuest').click(function(e){
+    e.preventDefault();
     $('<input class="w-100 f5 pv3 ph3 bg-light-gray bn" type="text" name="guest-name[]" value="" placeholder="Guest Name">').appendTo('.attending-guests .inputs');
   });
 
